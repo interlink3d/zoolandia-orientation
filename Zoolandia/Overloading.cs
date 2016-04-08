21eb1e3b3e1c93b351d9a94ad807295c4e005d53
Steve Brownlee <chortlehoort@gmail.com>
2016-04-08 08:38:48 -0500

    Separated different topics into different files

---
+++ Zoolandia/Overloading.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+class Animal
+{
+  private string _name = "";
+  private string _food = "";
+
+  public string species { get; set; }
+
+  public string name {
+    get
+    {
+      return _name;
+    }
+  }
+
+  public string food {
+    get
+    {
+      return _food;
+    }
+    set
+    {
+      if (value != "")
+      {
+        _food = value;
+      }
+    }
+  }
+
+  public virtual void eat(string food) {
+    _food = food;
+    Console.Write("Currently eating {0}", food);
+  }
+
+  public virtual void eat(string food, int count) {
+    _food = food;
+    Console.Write("Currently eating {0} {1}", count, food);
+  }
+
+
+}
+
+class Firefox : Animal {
+  public double furThickness { get; set; }
+  public int cutenessLevel { get; set; }
+
+  // Constructor function is responsible for
+  // executing initialization code
+  public Firefox (int cuteness)
+  {
+    cutenessLevel = cuteness;
+  }
+
+  public Firefox (double thickness)
+  {
+    furThickness = thickness;
+  }
+
+  public Firefox (int cuteness, double thickness)
+  {
+    cutenessLevel = cuteness;
+    furThickness = thickness;
+  }
+
+
+  // Overloaded method
+  public override void eat(string leaf)
+  {
+    if (leaf == "palm" || leaf == "bamboo" || leaf == "eucalyptus" || leaf == "willow")
+    {
+      food = leaf;
+      base.eat(leaf);
+    } else {
+      Console.WriteLine("Firefox can't eat that");
+    }
+  }
+
+  // Overloaded method
+  public override void eat(string leaf, int count)
+  {
+    if (count > 9)
+    {
+      if (leaf == "palm" || leaf == "bamboo" || leaf == "eucalyptus" || leaf == "willow")
+      {
+        food = leaf;
+        base.eat(leaf);
+      } else {
+        Console.WriteLine("Firefox can't eat that");
+      }
+    } else {
+      Console.WriteLine("Not enough food for Firefox");
+    }
+  }
+
+
+}
--- Zoolandia/Zoolandia.cs
+++ Zoolandia/Zoolandia.cs
@@ -1,37 +1,281 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+
+using System;
 
 namespace Zoolandia
 {
-    class Animal
-    {
-        private string _name = "";
 
-        public string species { get; set; }
+  class Animal
+  {
+    /*
+      Private scoped variable that are only accessible within
+      the Animal class.
+     */
+    private string _name = "";
+    private string _food = "";
+    private Species _species;
 
-        public string name {
-            get
-            {
-                return _name;
-            }
-        }
+    /*
+      Public property that is accessible by other classes but
+      can only be read, not manipulated.
+     */
+    public string name { get; set; }
 
-        public Animal(string name)
+    /*
+      Public property that is accessible by other classes that
+      can be read and set, but with some conditional logic that
+      checks the value before the private variable is set.
+
+      Animal adam = new Animal();
+      adam.food = "Banana";   <-- will compile
+      adam.food = "";         <-- will not compile
+     */
+    public string food {
+      get
+      {
+        return _food;
+      }
+      set
+      {
+        if (value != "")
         {
-            _name = name;
+          _food = value;
         }
+      }
     }
 
-    class Zoolandia
-    {
-        static void Main(string[] args)
-        {
-            Animal firstAnimal = new Animal(args[0]);
-            firstAnimal.species = "Ostrich";
-
-            Console.WriteLine("{0} the {1}", firstAnimal.name, firstAnimal.species);
-        }
+    /*
+      Your first method on a class.
+     */
+    public virtual void eat(string food) {
+      _food = food;
+      Console.WriteLine("Currently eating {0}", food);
     }
+  }
+
+  class Taxonomy
+  {
+    public url { get; set; }
+    public trait { get; set; }
+    public scientificName { get; set; }
+    public commonName { get; set; }
+  }
+
+  class Genus : Taxonomy
+  {
+
+  }
+
+  class Species : Taxonomy
+  {
+    public Genus genus { get; set; }
+  }
+
+
+  /*
+    Genus class
+   */
+  class Tenodera : Genus
+  {
+    private string _continent;
+
+    public string continent
+    {
+      get
+      {
+        return _continent;
+      }
+      set
+      {
+        if (value != "")
+        {
+          _continent = value;
+        }
+      }
+    }
+
+    private void init ()
+    {
+      this.url = "https://en.wikipedia.org/wiki/Tenodera";
+      this.trait = "apical spine";
+    }
+
+    public Tenodera () {
+      this.init();
+    }
+
+    public Tenodera (string continent) {
+      this.init();
+      this.continent = continent;
+    }
+  }
+
+  /*
+    Species of Tenodera
+   */
+  class TenoderaSinensis : Species
+  {
+    public TenoderaSinensis ()
+    {
+      this.continent = "Asia";
+      this.scientificName = "Tenodera sinensis";
+      this.commonName = "Chinese Mantis";
+    }
+  }
+
+  /*
+    Species of Tenodera
+   */
+  class TenoderaAngustipennis : Species
+  {
+    public TenoderaAngustipennis ()
+    {
+      this.continent = "Asia";
+      this.scientificName = "Tenodera angustipennis";
+      this.commonName = "Japanese Mantis";
+    }
+  }
+
+
+  /*
+    Genus class
+   */
+  class Procyon : Animal
+  {
+
+  }
+
+  /*
+    Species of Procyon
+   */
+  class ProcyonLotor : Procyon
+  {
+
+  }
+
+  /*
+    Species of Procyon
+   */
+  class ProcyonCancrivorus : Procyon
+  {
+
+  }
+
+  /*
+    Species of Procyon
+   */
+  class ProcyonPygmaeus : Procyon
+  {
+
+  }
+
+  /*
+    Species of Procyon
+   */
+  class ProcyonLotorGloveralleni : Procyon
+  {
+
+  }
+
+  /*
+    Species of Procyon
+   */
+  class ProcyonLotorMinor : Procyon
+  {
+
+  }
+
+  /*
+    Genus class
+   */
+  class Python : Animal
+  {
+
+  }
+
+  /*
+    Species of Python
+   */
+  class PythonCurtus : Python
+  {
+
+  }
+
+  /*
+    Species of Python
+   */
+  class PythonBrongersmai : Python
+  {
+
+  }
+
+  /*
+    Species of Python
+   */
+  class PythonBreitensteini : Python
+  {
+
+  }
+
+  /*
+    Species of Python
+   */
+  class PythonBivittatus : Python
+  {
+
+  }
+
+  /*
+    Species of Python
+   */
+  class PythonAnchietae : Python
+  {
+
+  }
+
+  /*
+    Genus class
+   */
+  class Loligo : Animal
+  {
+
+  }
+
+  /*
+    Species of Loligo
+   */
+  class LoligoForbesii : Loligo
+  {
+
+  }
+
+  /*
+    Species of Loligo
+   */
+  class LoligoReynaudii : Loligo
+  {
+
+  }
+
+  /*
+    Species of Loligo
+   */
+  class LoligoVulgaris : Loligo
+  {
+
+  }
+
+  class Zoolandia
+  {
+    static void Main(string[] args)
+    {
+      Animal firstAnimal = new Animal(args[0]);
+      firstAnimal.species = "Ostrich";
+
+      Console.WriteLine("{0} the {1}", firstAnimal.name, firstAnimal.species);
+    }
+  }
+
 }
+
+
